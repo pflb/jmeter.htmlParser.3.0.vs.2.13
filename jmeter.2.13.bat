01992d5926b444edd2faff7a8f55906d9487e0ac
Smirnov_VA <v.smirnov@pflb.ru>
2016-08-19 21:49:28 +0300

    Для корректного открытия csv-лога в стандартном Listener-е Apache.JMeter из имени колонки удалёна точка ".". Имя переменной "jmeter.version" заменено на "jmeterVersion". При наличии точни  в имени колонки лога, лог не открывался в JMeter.

--- jmeter.2.13.bat
+++ jmeter.2.13.bat
@@ -5,7 +5,7 @@ SET jmeter="D:\tools\jmeter.2.13\bin\jmeter" --testfile %testfile% ^
 	--jmeterproperty URL=%1 ^
 	--jmeterproperty loopCount=%2 ^
 	--jmeterproperty siteKey=%3 ^
-	-Jsample_variables=siteKey,htmlParser,jmeter.version,i ^
+	-Jsample_variables=siteKey,htmlParser,jmeterVersion,i ^
 	-Dfile.encoding=UTF-8
 
 CALL %jmeter% --jmeterproperty htmlParser.className=org.apache.jmeter.protocol.http.parser.LagartoBasedHtmlParser  --jmeterlogfile jmeterLogs\jmeter.2.13.LagartoBasedHtmlParser.%3.log
